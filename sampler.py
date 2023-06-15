afe0d1a9561d0754044b8a39e3a7dcf729df30b4
Chengbing Wang <102858459+Wcb0219@users.noreply.github.com>
2023-06-15 21:46:56 +0800

    Debug sampler.py

--- sampler.py
+++ sampler.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 import math
-
+######################################
 class base_sampler(nn.Module):
     """
     Uniform sampler
